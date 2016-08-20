5a69d672104f564c72a284edf1a06351771e2174
Daroc Alden <setupminimal@gmail.com>
2016-08-20 13:35:31 -0400

    Useability.

--- projector.py
+++ projector.py
@@ -26,32 +26,35 @@ def linkProject(project, exe):
 
 
 if __name__ == "__main__":
-   args = sys.argv[1:]
-   while args != []:
-       if args[0] == "-w":
-           WORKSPACE=args[1]
-           args = args[2:]
-       elif args[0] == "-e":
-           EXECUTABLE_LOCATION=args[1]
-           args = args[2:]
-       elif args[0] == "link":
-           projectName = args[1]
-           exeName = args[2]
-           linkProject(projectName, exeName)
-           args = args[3:]
-       elif args[0] == "help" or args[0] == "--help" or args[0] == "-h":
-           print "Projector: Simple Project Management"
-           print "-h : help"
-           print "-w : Set workspace"
-           print "-e : Set executable location"
-           print ""
-           print "create (default) : Create a new project"
-           print "link             : Link an executable from a project"
-           print ""
-           print "Projector - Accept no Substitutes"
-       elif args[0] == "create":
-           createProject(args[1])
-           args = [2:]
-       else:
-           createProject(args[0])
-           args = args[1:]
+    args = sys.argv[1:]
+#    print args
+#    sys.exit(0)
+    while args != []:
+        if args[0] == "-w":
+            WORKSPACE=args[1]
+            args = args[2:]
+        elif args[0] == "-e":
+            EXECUTABLE_LOCATION=args[1]
+            args = args[2:]
+        elif args[0] == "link":
+            projectName = args[1]
+            exeName = args[2]
+            linkProject(projectName, exeName)
+            args = args[3:]
+        elif args[0] == "help" or args[0] == "--help" or args[0] == "-h":
+            print "Projector: Simple Project Management"
+            print "-h : help"
+            print "-w : Set workspace"
+            print "-e : Set executable location"
+            print ""
+            print "create (default) : Create a new project"
+            print "link             : Link an executable from a project"
+            print ""
+            print "Projector - Accept no Substitutes"
+            args = args[1:]
+        elif args[0] == "create":
+            createProject(args[1])
+            args = args[2:]
+        else:
+            createProject(args[0])
+            args = args[1:]
